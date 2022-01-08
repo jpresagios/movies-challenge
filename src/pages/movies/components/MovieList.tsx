a96e32841f4ffe3f8c7c441eaab15edf0e3d9d3a
jpresagios <junior.presagios@gmail.com>
2022-01-08 23:03:31 +0200

    Added MovieList to show home movies

---
+++ src/pages/movies/components/MovieList.tsx
@@ -0,0 +1,37 @@
+import {
+  Container,
+  Grid,
+} from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import MovieItem from './MovieItem';
+import Movie from '../../../interfaces/movie';
+import { getMovies, moviesSelector } from '../../../slices/moviesSlice';
+
+export default function MovieList() {
+  const { movies } = useSelector(moviesSelector);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMovies());
+  }, [dispatch]);
+
+  return (
+    <Container maxWidth={false}>
+      <Grid container spacing={4}>
+        {movies?.map((item: Movie) => (
+
+          <Grid item xs={12} sm={6} md={3} key={item._id}>
+
+            <MovieItem
+              title={item.title}
+              description={item.description}
+              url={item.images.posterArt.url}
+            />
+
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
