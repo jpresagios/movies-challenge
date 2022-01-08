af1b23628e9f33ab9bcb5667e7baec504b25b64d
jpresagios <junior.presagios@gmail.com>
2022-01-09 01:39:56 +0200

    Include MovieSearch on MovieList
    This component is used to searching

--- src/pages/movies/components/MovieList.tsx
+++ src/pages/movies/components/MovieList.tsx
@@ -1,12 +1,10 @@
-import {
-  Container,
-  Grid,
-} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import MovieItem from './MovieItem';
 import Movie from '../../../interfaces/movie';
 import { getMovies, moviesSelector } from '../../../slices/moviesSlice';
+import MovieSearch from './MovieSearch';
 
 export default function MovieList() {
   const { movies } = useSelector(moviesSelector);
@@ -17,21 +15,19 @@ export default function MovieList() {
   }, [dispatch]);
 
   return (
-    <Container maxWidth={false}>
+    <>
+      <MovieSearch />
       <Grid container spacing={4}>
         {movies?.map((item: Movie) => (
-
           <Grid item xs={12} sm={6} md={3} key={item._id}>
-
             <MovieItem
               title={item.title}
               description={item.description}
               url={item.images.posterArt.url}
             />
-
           </Grid>
         ))}
       </Grid>
-    </Container>
+    </>
   );
 }
