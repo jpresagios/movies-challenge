9eefb72ccf28603acb67108279bdd3f9ba5cf944
jpresagios <junior.presagios@gmail.com>
2022-01-08 23:42:56 +0200

    Added NavBar component

---
+++ src/pages/common/NavBar.tsx
@@ -0,0 +1,53 @@
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import MenuIcon from '@material-ui/icons/Menu';
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function UserLayout() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Movies App
+            </Typography>
+            <Typography variant="h6"> Welcome</Typography>
+            <IconButton
+              arial-label="exit-button"
+              color="inherit"
+            >
+              <ExitToAppIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </div>
+  );
+}
