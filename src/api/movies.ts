8f0acc1faf0838e486861f2394850f93a0a210d2
jpresagios <junior.presagios@gmail.com>
2022-01-09 10:49:40 +0200

    Update date fetching support (sorting, filtering)

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,8 +1,28 @@
 import axios from './axiosClient';
 import Movie from '../interfaces/movie';
+import SearchFields from '../interfaces/searchFields';
+import SortFields from '../interfaces/sortFields';
 
-const fetchMovies = async (): Promise<Movie[]> => {
-  const { data } = await axios.get('');
+const fetchMovies = async (
+  searchFields: SearchFields,
+  sortFields: SortFields,
+): Promise<Movie[]> => {
+  const { type, year } = searchFields;
+  const { field, order } = sortFields;
+
+  let url = `?_sort=${field}&_order=${order}`;
+
+  const additionalFilter = [{ fieldFilter: 'programType', valueFilter: type }, { field: 'releaseYear', value: year }];
+
+  for (let i = 0; i < additionalFilter.length; i += 1) {
+    const { fieldFilter, valueFilter } = additionalFilter[i];
+
+    if (valueFilter) {
+      url += `&${fieldFilter}=${valueFilter}`;
+    }
+  }
+
+  const { data } = await axios.get(url);
   return data;
 };
 
