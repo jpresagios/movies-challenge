b4297af212b1e8bfad1c692f6080a496919e8ef2
jpresagios <junior.presagios@gmail.com>
2022-01-09 18:02:08 +0200

    Added authSlice to the store

--- src/store.ts
+++ src/store.ts
@@ -2,11 +2,13 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import movieSlice from './state/slices/moviesSlice';
+import authSlice from './state/slices/authSlice';
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunk));
 
 const rootReducer = combineReducers({
   movieStore: movieSlice,
+  authStore: authSlice,
 });
 
 const store = createStore(rootReducer, composedEnhancer);
