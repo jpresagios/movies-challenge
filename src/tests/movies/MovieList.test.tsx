091a5bc384d1b332de0697ae7f90ae6010b227b8
jpresagios <junior.presagios@gmail.com>
2022-01-09 23:47:25 +0200

    Added test Movie List render all the movies

---
+++ src/tests/movies/MovieList.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { mount } from 'enzyme';
+import moviesMock from './mockData';
+import App from '../../App';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+interface Text {
+  text: string;
+}
+
+jest.mock('react-text-truncate', () => function truncate({ text }: Text) {
+  return <div>{text}</div>;
+});
+
+test('Movie List render all the movies', () => {
+  const initState = {
+    movieStore: {
+      movies: moviesMock,
+      sortFields: { field: 'releaseYear', order: 'desc' },
+      searchFields: { year: 0, type: '' },
+      loadingMovies: false,
+      errors: '',
+    },
+  };
+
+  const store = mockStore(initState);
+
+  const wrapper = mount(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+
+  for (let i = 0; i < moviesMock.length; i += 1) {
+    expect(wrapper.text().includes(moviesMock[i].title)).toBe(true);
+  }
+});
