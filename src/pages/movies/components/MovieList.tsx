6d9fa12139a341ed244e9aa1b41a8c250cf968c4
jpresagios <junior.presagios@gmail.com>
2022-01-09 21:57:43 +0200

    Move movies data fetching to container

--- src/pages/movies/components/MovieList.tsx
+++ src/pages/movies/components/MovieList.tsx
@@ -1,29 +1,27 @@
 import { CircularProgress, Grid } from '@material-ui/core';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import React from 'react';
 import MovieItem from './MovieItem';
 import Movie from '../../../interfaces/movie';
-import { getMovies, moviesSelector } from '../../../state/slices/moviesSlice';
 import MovieSearch from './MovieSearch';
 import MovieSort from './MovieSort';
 
-export default function MovieList() {
-  const {
-    movies, searchFields, sortFields, loadingMovies,
-  } = useSelector(moviesSelector);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getMovies());
-  }, [dispatch, searchFields, sortFields]);
-
+interface MovieListProps {
+  loadingMovies: boolean;
+  movies: Movie[];
+}
+export default function MovieList({ loadingMovies, movies }: MovieListProps) {
   return (
     <>
       {loadingMovies && (
-      <Grid container justifyContent="center" alignItems="center" style={{ margin: '0 auto' }}>
-        <CircularProgress size={40} />
-      </Grid>
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          style={{ margin: '0 auto' }}
+        >
+          <CircularProgress size={40} />
+        </Grid>
       )}
 
       {!loadingMovies && (
@@ -33,7 +31,10 @@ export default function MovieList() {
           <Grid container spacing={4}>
             {movies?.map((item: Movie) => (
               <Grid item xs={12} sm={12} md={6} lg={3} key={item._id}>
-                <Link to={`movie/${item._id}`} style={{ textDecoration: 'none' }}>
+                <Link
+                  to={`movie/${item._id}`}
+                  style={{ textDecoration: 'none' }}
+                >
                   <MovieItem
                     title={item.title}
                     description={item.description}
--- src/pages/movies/containers/MovieContainer.tsx
+++ src/pages/movies/containers/MovieContainer.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import MovieList from '../components/MovieList';
+import { getMovies, moviesSelector } from '../../../state/slices/moviesSlice';
 
 export default function MovieContainer() {
+  const {
+    movies, searchFields, sortFields, loadingMovies,
+  } = useSelector(moviesSelector);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMovies());
+  }, [dispatch, searchFields, sortFields]);
+
   return (
     <div style={{ padding: 40 }}>
-      <MovieList />
+      <MovieList movies={movies} loadingMovies={loadingMovies} />
     </div>
   );
 }
