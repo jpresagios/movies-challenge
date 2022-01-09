11414432ff4f9f7992123a339fc0a4ee9e0511a5
jpresagios <junior.presagios@gmail.com>
2022-01-09 20:21:36 +0200

    Added MovieDetail component
    USe to show movie detail

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 import MovieContainer from './pages/movies/containers/MovieContainer';
 import NavBar from './pages/common/NavBar';
 import LoginContainer from './pages/auth/containers/LoginContainer';
+import MovieDetail from './pages/movies/components/MovieDetail';
 
 function App() {
   return (
@@ -15,6 +16,12 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<MovieContainer />} />
+          <Route
+            path="/movie/:id"
+            element={
+              <MovieDetail />
+}
+          />
           <Route path="/login" element={<LoginContainer />} />
         </Routes>
       </div>
